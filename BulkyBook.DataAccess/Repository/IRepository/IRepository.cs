db9de4048161b55e6326f772c49d6a3346299670
zachary-thomas <zachy.thomas@gmail.com>
2020-07-20 20:59:30 -0400

    Repository Interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>>
+            )
+    }
+}
---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        // 'Id' will implicitly be a primary key without an annotation
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
