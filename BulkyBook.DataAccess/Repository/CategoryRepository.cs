0def9a252ee34c02239eddf2fc40d5b09fc1d5e8
zachary-thomas <zachy.thomas@gmail.com>
2020-07-21 22:59:47 -0400

    Implement repository interface

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -10,7 +11,22 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         T Get(int id);
 
         IEnumerable<T> GetAll(
-            Expression<Func<T, bool>>
-            )
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
     }
 }
