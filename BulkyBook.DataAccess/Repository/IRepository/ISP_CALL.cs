bc3ec2980eff6e5386bc8fe870f3769ad6119ce6
zachary-thomas <thomazac@mail.gvsu.edu>
2020-07-23 17:25:28 -0400

    Implement Stored Procedures

---
+++ BulkyBook.DataAccess/Repository/IRepository/ISP_CALL.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    //ISP -> Stored Procedures
+    public interface ISP_CALL : IDisposable
+    {
+        // Using Dapper to map the objects from the DB into our models
+        T Single<T>(string procedureName, DynamicParameters param = null);
+
+        void Execute(string procedureName, DynamicParameters param = null);
+
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
+    }
+}
