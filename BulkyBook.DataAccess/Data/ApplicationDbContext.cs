077b91a9eac4a7ed3cb820ca70edd970eb911d8e
Zac <thomazac@mail.gvsu.edu>
2020-12-20 11:55:37 -0500

    add product model and product table to db

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,6 @@ namespace BulkyBook.DataAccess.Data
         // Define that Category will go into the database
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
