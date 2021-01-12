c483e7c988f7a36248d44f6a50cde83ebf1a4031
Zac <thomazac@mail.gvsu.edu>
2021-01-11 20:13:02 -0500

    completed email integration

---
+++ BulkyBook.Utility/SecretProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class SecretProperties
+    {
+        public string FacebookAppId => "445390703303476";
+        public string FacebookSecret => "7c7c5d9ed615be854c1e8ed8d0e84631";
+
+        public string GoogleClientId => "900278809779-eferl6ra529kcomrie95n893n76nv8e7.apps.googleusercontent.com";
+        public string GoogleClientSecret => "yE04fhWPGdPHiqb0Vy1D-o4J";
+    }
+}
