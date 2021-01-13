fb15f136fe8fb390d34ee5efe3843972ee5c8e25
Zac <thomazac@mail.gvsu.edu>
2021-01-12 19:18:04 -0500

    add order details, order header, and shopping cart models and repos

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -11,6 +11,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
         IApplicationUserRepository ApplicationUser { get; }
+        IOrderDetailsRepository OrderDetails { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        IShoppingCartRepository ShoppingCart { get; }
         ISP_Call SP_Call { get; }
 
         void Save();
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -14,6 +14,9 @@ namespace BulkyBook.DataAccess.Repository
         public ICoverTypeRepository CoverType { get; private set; }
         public ICompanyRepository Company { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderDetailsRepository OrderDetails { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
@@ -24,6 +27,9 @@ namespace BulkyBook.DataAccess.Repository
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderDetails = new OrderDetailsRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
