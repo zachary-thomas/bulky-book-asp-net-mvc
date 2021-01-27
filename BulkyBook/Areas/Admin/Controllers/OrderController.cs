01105af97301ee84935e8c3068b795d1eb179735
Zac <thomazac@mail.gvsu.edu>
2021-01-27 18:35:08 -0500

    start on orders index for admin

---
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetOrderList()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            orderHeaders = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
