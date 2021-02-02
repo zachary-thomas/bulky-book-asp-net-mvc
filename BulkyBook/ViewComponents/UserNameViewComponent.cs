52dc4f6f6c3e3d79955fd6579ef912ba0e9fce39
Zac <thomazac@mail.gvsu.edu>
2021-02-02 18:40:22 -0500

    add view component to show user's name in _Layout

---
+++ BulkyBook/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BulkyBook.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return View(unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claims.Value));
+
+        }
+    }
+}
